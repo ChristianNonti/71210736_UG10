882c9afd03e958665cef05e6f3ac62f64052a8da
ChristianNonti <93360281+ChristianNonti@users.noreply.github.com>
2022-11-24 16:14:36 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,7 @@
+nama_pemain1 = input("Masukan nama pemain pertama :")
+nama_pemain2 = input("Masukan nama pemain kedua :")
+nama_pemain3 = input("Masukan nama pemain ketiga :")
+
+kartu1 = input("Masukan jumlah kartu pemain pertama :")
+kartu2 = input("Masukan jumlah kartu pemain kedua :")
+kartu3 = input("Masukkan jumlah karti pemain ketiga :")
