4007378ae333b6188089b15b8a99ca5495e97dc2
ChristianNonti <93360281+ChristianNonti@users.noreply.github.com>
2022-11-24 16:08:46 +0700

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,12 @@
+input = input("Masukan tahun :")
+
+if input == '2000':
+    print("2000 merupakan tahun kabisat")
+elif input == '2002':
+    print("2002 bukan merupakan tahun kabisat")
+elif input == '2003':
+    print("2003 bukan merupakan tahun kabisat")
+elif input == '2020':
+    print("2002 merupakan tahun kabisat")
+else:
+    print("inputan salah")
